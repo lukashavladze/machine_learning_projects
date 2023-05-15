b8af495620714b993966b115dd5ccc43675a5e50
Luka <lukianeshavladze@gmail.com>
2023-05-15 20:27:02 +0400

    linear regression model

--- linear_regression.py
+++ linear_regression.py
@@ -10,3 +10,9 @@ print(customers.head())
 
 # using seaborn to create jointplot to compare the Time on Website and  Yearly Amount Spent columns.
 sns.jointplot(data=customers, x='Time on Website', y='Yearly Amount Spent')
+plt.show()
+
+# using jointplot to create 2D hex bin plot comparing Time on App and Length of Membership
+sns.jointplot(x='Time on App', y='Length of Membership', kind='hex', data=customers)
+plt.show()
+
